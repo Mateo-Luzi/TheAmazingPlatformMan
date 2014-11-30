18d9c241ca10e9494bbd5785384572fde1dd9a42
Mateo-Luzi <luzima49053@th-nuernberg.de>
2014-11-30 17:31:14 +0100

    remove unnecessary trampoline velocity change, slightly increase trampoline jump penalty

--- Assets/trampolineSpawn.cs
+++ Assets/trampolineSpawn.cs
@@ -10,7 +10,7 @@ public class trampolineSpawn : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		jumpCount = 2;
-		Destroy (gameObject, 10);
+		Destroy (gameObject, 10.0f);
 	}
 	
 	// Update is called once per frame
@@ -23,13 +23,11 @@ public class trampolineSpawn : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D  col)
 	{
 		if (col.gameObject.tag == "Player") {
-			col.gameObject.rigidbody2D.velocity = new Vector3 (col.gameObject.rigidbody2D.velocity.x, 0);
 			col.gameObject.rigidbody2D.velocity = new Vector2(col.gameObject.rigidbody2D.velocity.x,100);
-
 			audio.PlayOneShot (trampolineSound);
 
 			if (jumpCount > 0) {
-					transform.localScale = new Vector3 (transform.localScale.x * 0.5f, transform.localScale.y * 0.5f, transform.localScale.z * 0.5f);
+					transform.localScale = new Vector3 (transform.localScale.x * 0.45f, transform.localScale.y * 0.45f, transform.localScale.z * 0.45f);
 					jumpCount--;
 			} else
 					Destroy (gameObject, 0.25f);
