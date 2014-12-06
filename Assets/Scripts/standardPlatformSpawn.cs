668eb1ec405dd7600a12b765e1c0f01789f782f6
Mateo-Luzi <luzima49053@th-nuernberg.de>
2014-12-06 01:34:45 +0100

    add jump-through feature for standard platforms [experimental], make physics engine refresh twice as fast as before

--- Assets/Scripts/standardPlatformSpawn.cs
+++ Assets/Scripts/standardPlatformSpawn.cs
@@ -3,11 +3,26 @@ using System.Collections;
 
 public class standardPlatformSpawn : MonoBehaviour {
 
-
-
+	private SpaceMarineController player;
+	BoxCollider2D[] playerBoxColliderList; 
+	CircleCollider2D[] playerCircleColliderList;
+	Vector3 distanceFromPlayer;
 	// Use this for initialization
 	void Start () {
-		Destroy (gameObject, 10);
+
+		// experimental jump-through code
+		player = GameObject.Find ("Character").GetComponent<SpaceMarineController> ();
+		playerBoxColliderList = player.gameObject.GetComponents<BoxCollider2D> ();
+		playerCircleColliderList = player.gameObject.GetComponents<CircleCollider2D> ();
+		distanceFromPlayer =  transform.position - player.transform.position;
+	
+
+		// dont let player spawn platform inside himself
+		// good luck making this work
+//		if ((distanceFromPlayer.y < 3.5 && distanceFromPlayer.y > -5) && (distanceFromPlayer.x < -2 && distanceFromPlayer.y > 2))
+//			Destroy (gameObject);
+
+		Invoke ("DestroyPlatform", 10.0f);
 	}
 	
 	// Update is called once per frame
@@ -16,12 +31,42 @@ public class standardPlatformSpawn : MonoBehaviour {
 
 	}
 
-	void OnTriggerEnter2D(Collider2D  col)
-	{
+	void OnTriggerEnter2D(Collider2D  col){
 		if (col.gameObject.tag == "Player") {
-			Destroy (gameObject, 2.0f);	
+			Invoke ("DestroyPlatform", 2.0f);
 		}
 		if (col.gameObject.tag == "Ground")
 			Destroy(gameObject);
+
+
+		// experimental jump-through code
+		CancelInvoke("DestroyPlatform");
+		if(!player.grounded || distanceFromPlayer.y > 0.0f )
+			foreach (BoxCollider2D b in playerBoxColliderList) 
+				b.isTrigger = true;		
+		if(!player.grounded)
+			foreach (CircleCollider2D c in playerCircleColliderList) 
+				c.isTrigger = true;	
+
+
 	}
+
+	// experimental jump-through code
+	void OnTriggerExit2D(Collider2D  col){
+		foreach (BoxCollider2D b in playerBoxColliderList) 
+			b.isTrigger = false;		
+		foreach (CircleCollider2D c in playerCircleColliderList) 
+			c.isTrigger = false;
+
+		player.grounded = true;
+		Invoke ("DestroyPlatform", 2.0f);
+	}
+
+
+	void DestroyPlatform() {
+		Destroy (gameObject);
+	}
+
+
+
 }
