9279d1ef0910bfdd7f6e9d5cc2a48015112dbab2
Andi <andi-boegelein@web.de>
2014-12-15 21:13:58 +0100

    highlight selected platform; main menu option "start game" now starts scene "level-1-0"

--- Assets/Scripts/ingameHud.cs
+++ Assets/Scripts/ingameHud.cs
@@ -5,12 +5,21 @@ using System.Collections;
 public class ingameHud : MonoBehaviour {
 
 	private SpaceMarineController player;
+	public Image standard;
+	public Image trampoline;
+	public Image booster;
+	public Color standardColor;
+	public Color trampolineColor;
+	public Color boosterColor;
 	public Text trampolineAmmoDisplay;
 	public Text boosterAmmoDisplay;
 	public Text timeDisplay;
 
 	// Use this for initialization
 	void Start () {
+		standardColor = standard.color;
+		trampolineColor = trampoline.color;
+		boosterColor = booster.color;
 	}
 	
 	// Update is called once per frame
@@ -25,6 +34,27 @@ public class ingameHud : MonoBehaviour {
 
 		trampolineAmmoDisplay.text = player.trampolineAmmo + "/" + player.maxAmmo;
 		boosterAmmoDisplay.text = player.boosterAmmo + "/" + player.maxAmmo;
+
+
+		switch (player.platformMode) {
+		case 1:
+			standard.color = standardColor;
+			trampoline.color = Color.grey;
+			booster.color = Color.grey;
+			break;
+		case 2:
+			standard.color = Color.grey;
+			trampoline.color = trampolineColor;
+			booster.color = Color.grey;
+			break;
+		case 3:
+			standard.color = Color.grey;
+			trampoline.color = Color.grey;
+			booster.color = boosterColor;
+			break;
+		default:
+			break;
+		}
 
 
 	}
