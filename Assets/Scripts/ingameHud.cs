03dc0668c2255dc95f79322086d792888f0d1c37
Andi <andi-boegelein@web.de>
2014-12-15 18:43:16 +0100

    added HUD as prefab

---
+++ Assets/Scripts/ingameHud.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ingameHud : MonoBehaviour {
+
+	private SpaceMarineController player;
+	public Text trampolineAmmoDisplay;
+	public Text boosterAmmoDisplay;
+	public Text timeDisplay;
+	public float timeElapsed;
+
+	// Use this for initialization
+	void Start () {
+		timeElapsed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeElapsed = Time.time;
+		timeDisplay.text = timeElapsed.ToString("F2");
+
+		if (player == null) {
+			player = GameObject.FindGameObjectWithTag ("Player").GetComponent<SpaceMarineController> ();
+			if (player == null)
+				return;
+		}
+
+		trampolineAmmoDisplay.text = player.trampolineAmmo + "/" + player.maxAmmo;
+		boosterAmmoDisplay.text = player.boosterAmmo + "/" + player.maxAmmo;
+
+
+	}
+	
+}
