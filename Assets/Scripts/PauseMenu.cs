6682cf4963e3871acf9780be9f1c44696055d256
Andi <andi-boegelein@web.de>
2014-12-16 22:45:19 +0100

    fixed menus

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,6 +5,7 @@ public class PauseMenu : MonoBehaviour {
 
 	private SpaceMarineController player;
 	private Rect windowRect;
+	private Exit exit;
 	public bool pause;
 
 	// Use this for initialization
@@ -21,19 +22,25 @@ public class PauseMenu : MonoBehaviour {
 			if (player == null)
 				return;
 		}
-
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			if(pause == true)
-				pause = false;
-			else
-				pause = true;
-	
+		if (exit == null) {
+			exit = GameObject.FindGameObjectWithTag ("ExitDoor").GetComponent<Exit> ();
+			if (exit == null)
+				return;
 		}
 
-		if (pause == true) 
-						pauseGame ();
-				else
-						runGame ();
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if(exit.finished == true)
+				return;
+			if(pause == true){
+				pause = false;
+				runGame ();
+			}
+			else{
+				pause = true;
+				pauseGame ();
+			}
+	
+		}						
 	}
 
 	void OnGUI(){
@@ -42,12 +49,19 @@ public class PauseMenu : MonoBehaviour {
 	}
 
 	void windowFunc(int id){
-		if (GUILayout.Button ("Resume"))
+		if (GUILayout.Button ("Resume")) {
 						pause = false;
-		if(GUILayout.Button ("Restart Level"))
-			Application.LoadLevel (Application.loadedLevelName);
-		if (GUILayout.Button ("Exit to Main Menu"))
-						Application.LoadLevel ("mainMenu");
+						runGame();
+				}
+		if (GUILayout.Button ("Restart Level")) {
+						Application.LoadLevel (Application.loadedLevelName);
+						pause = false;
+						runGame();
+				}
+		if (GUILayout.Button ("Exit to Main Menu")) {
+			Application.LoadLevel ("mainMenu");
+			Time.timeScale = 1;		
+		}
 		if (GUILayout.Button ("Exit Game"))
 						Application.Quit ();
 
