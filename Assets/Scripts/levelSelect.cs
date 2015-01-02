b3e4481236ed6d45e045a9233c53393387cdff9f
Andi <andi-boegelein@web.de>
2015-01-03 00:36:47 +0100

    added level selection screen

---
+++ Assets/Scripts/levelSelect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelSelect : MonoBehaviour {
+
+	void Update()	{
+		Screen.showCursor = true;
+	}
+
+	public void ChangeToScene(string sceneToChangeTo){
+		Application.LoadLevel(sceneToChangeTo);
+	}
+
+	public void BackToMainMenu(){
+		Application.LoadLevel("mainMenu");
+	}
+}
