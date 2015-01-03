72ed27dcb5753d2f18174615e4b257e769d7a0f8
Mateo-Luzi <luzima49053@th-nuernberg.de>
2015-01-03 21:25:15 +0100

    fade standard platforms on destruction, optimize camera for high-velocity situations, add unique background for each level, add player halo

--- Assets/Scripts/SmoothFollow.js
+++ Assets/Scripts/SmoothFollow.js
@@ -10,11 +10,20 @@ var velocity : Vector2;
 private var smoothTimeX : float = 0.3;
 private var smoothTimeY : float = 0.3;
 
+private var standardCameraSize : float;
+private var currentCameraSize : float;
+
 
 private var thisTransform : Transform;
 	function Start ()
 	{
 		thisTransform = transform;
+		 standardCameraSize = thisTransform.camera.orthographicSize;
+		 
+		 for (var child : Transform in transform) {
+			     child.transform.localScale.x = child.transform.localScale.x * 1.4;
+			     child.transform.localScale.y = child.transform.localScale.y * 1.4;
+			}
 
 	}
 	function FixedUpdate ()
@@ -32,11 +41,13 @@ private var thisTransform : Transform;
 		if (cameraFollowX){
 
 			thisTransform.position.x = Mathf.SmoothDamp (thisTransform.position.x , cameraTarget.transform.position.x, velocity.x, smoothTimeX);
+			thisTransform.camera.orthographicSize = Mathf.SmoothDamp(thisTransform.camera.orthographicSize, Mathf.Exp(Mathf.Abs(cameraTarget.transform.rigidbody2D.velocity.x)/35) + standardCameraSize, currentCameraSize, 0.66f);
 		}
 		 
 		if (cameraFollowY){
 			smoothTimeY = (velocity.y)/100;		
 			thisTransform.position.y = Mathf.SmoothDamp (thisTransform.position.y , cameraTarget.transform.position.y , velocity.y, smoothTimeY);
+			thisTransform.camera.orthographicSize = Mathf.SmoothDamp(thisTransform.camera.orthographicSize, Mathf.Exp(Mathf.Abs(cameraTarget.transform.rigidbody2D.velocity.y)/30) + standardCameraSize, currentCameraSize, 0.66f);
 		}
 		if (!cameraFollowX && cameraFollowHeight)
 		{
--- Assets/Scripts/standardPlatformSpawn.cs
+++ Assets/Scripts/standardPlatformSpawn.cs
@@ -7,27 +7,40 @@ public class standardPlatformSpawn : MonoBehaviour {
 	BoxCollider2D[] playerBoxColliderList; 
 	CircleCollider2D[] playerCircleColliderList;
 	Vector3 distanceFromPlayer;
+	Color defaultColor;
+	float destroyDuration = 1.5f;
+	bool fading = false;
+	float lerp;
+	Color colorFaded;
+
 	// Use this for initialization
 	void Start () {
-		Invoke ("DestroyPlatform", 10.0f);
+		Invoke ("DestroyPlatform", 8.0f);
+		defaultColor = gameObject.renderer.material.color;
+		colorFaded = new Color (defaultColor.r, defaultColor.g, defaultColor.b, 0);
+		lerp = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-
+		if (fading) {
+			lerp += Time.deltaTime / destroyDuration;
+			renderer.material.color = Color.Lerp (defaultColor, colorFaded, lerp);
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D  col){
 		if (col.gameObject.tag == "Player") {
-			Invoke ("DestroyPlatform", 2.0f);
+			Invoke ("DestroyPlatform", destroyDuration);
+			fading = true;
 		}
 		if (col.gameObject.tag == "Ground")
 			Destroy(gameObject);
 	}
 	
 	void DestroyPlatform() {
-		Destroy (gameObject);
+		fading = true;
+		Destroy (gameObject, 2.0f);
 	}
 
 
