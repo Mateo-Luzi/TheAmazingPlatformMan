fa203f81d70eebb1b56e3f59935b1fba49e47f1f
Andi <andi-boegelein@web.de>
2015-01-05 00:24:44 +0100

    added audio clips for clicking

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class mainMenu : MonoBehaviour {
 
+	public AudioClip selection;
+	public AudioClip deselection;
+
 	void Update()	{
 		Screen.showCursor = true;
 	}
 
 	public void ChangeToScene(string sceneToChangeTo){
+		audio.PlayOneShot (selection);
 		Application.LoadLevel(sceneToChangeTo);
 	}
 
 	public void ExitGame(){
+		audio.PlayOneShot (deselection);
 		Application.Quit();
 	}
 }
